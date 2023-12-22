617a9b1e68802cc45551d1e507068c0e40ba8af5
Edward Samlafo-Adams <edwardmawuko75@gmail.com>
2023-12-22 17:23:05 +0100

    added hover effect to the grid lines

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const gridContainer = document.querySelector(".grid-container");
 
   // Create 16x16 grid items
-  for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridContainer.appendChild(gridItem);
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridContainer.appendChild(gridItem);
+    }
   }
 });
