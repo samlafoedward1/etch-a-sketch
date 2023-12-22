6c4a92c40a489dc2d9f02fecb165ea1f146ca4c1
Edward Samlafo-Adams <edwardmawuko75@gmail.com>
2023-12-22 17:12:44 +0100

    made 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gridContainer = document.querySelector(".grid-container");
+
+  // Create 16x16 grid items
+  for (let i = 0; i < 16 * 16; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridContainer.appendChild(gridItem);
+  }
+});
