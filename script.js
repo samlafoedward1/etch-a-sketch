6d5003bfd64b67f080470f5c5998527ccdca24cb
Edward Samlafo-Adams <edwardmawuko75@gmail.com>
2023-12-22 17:34:21 +0100

    modifying hovering effect

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
       const gridItem = document.createElement("div");
       gridItem.classList.add("grid-item");
       gridContainer.appendChild(gridItem);
+
+      //setting color for hover over grids
+      gridItem.addEventListener("mouseover", function () {
+        gridItem.style.backgroundColor = " lightblue";
+      });
+
+      //reset grid color
+      gridItem.addEventListener("mouseout", function () {
+        gridItem.style.backgroundColor = "";
+      });
     }
   }
 });
