02b6bb0a895d8dcd5d1809d516fbd094944552e4
Edward Samlafo-Adams <edwardmawuko75@gmail.com>
2023-12-23 12:58:39 +0100

    modifying sketchpad

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-"use strict";
-
 document.addEventListener("DOMContentLoaded", function () {
+  let numRows, numCols; // Declare variables for both rows and columns
+
   const gridMaking = document.querySelector(".input-grid");
   gridMaking.addEventListener("click", function () {
-    const newGrid = prompt("Input grid sides");
+    numRows = prompt("Input the number of rows");
+    numCols = prompt("Input the number of columns");
+
+    // Validate user input (check if they are valid numbers)
+    if (isValidInput(numRows) && isValidInput(numCols)) {
+      createGrid(numRows, numCols); // Call the createGrid function with user input
+    } else {
+      alert("Please enter valid numbers for rows and columns.");
+    }
   });
 
-  const gridContainer = document.querySelector(".grid-container");
+  function isValidInput(value) {
+    return !isNaN(value) && value > 0; // Check if the input is a number greater than 0
+  }
 
-  // Create 16x16 grid items
-  for (let i = 0; i < newGrid; i++) {
-    for (let j = 0; j < newGrid; j++) {
-      const gridItem = document.createElement("div");
-      gridItem.classList.add("grid-item");
-      gridContainer.appendChild(gridItem);
+  function createGrid(rows, cols) {
+    const gridContainer = document.querySelector(".grid-container");
 
-      //setting color for hover over grids
-      gridItem.addEventListener("mouseover", function () {
-        gridItem.style.backgroundColor = " lightblue";
-      });
+    // Clear previous grid items
+    gridContainer.innerHTML = "";
 
-      //   reset grid color
-      gridItem.addEventListener("mouseout", function () {
-        gridItem.style.backgroundColor = "";
-      });
+    // Create grid
+    if (rows < 100 && cols < 100) {
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+          const gridItem = document.createElement("div");
+          gridItem.classList.add("grid-item");
+          gridContainer.appendChild(gridItem);
+
+          // Set color for hover over grids
+          gridItem.addEventListener("mouseover", function () {
+            gridItem.style.backgroundColor = "lightblue";
+          });
+
+          // Reset grid color
+          gridItem.addEventListener("mouseout", function () {
+            gridItem.style.backgroundColor = "";
+          });
+        }
+      }
+    } else {
+      alert("Enter numbers below 100");
     }
   }
 });
